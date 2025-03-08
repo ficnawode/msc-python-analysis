3e61027cbcfa79dfe3471c87cf46dd604ddea410
Tobiasz Fic <tobi.fic@gmail.com>
2025-03-08 15:34:32 +0100

    Add I V characteristic

---
+++ I_V_characteristic.py
@@ -0,0 +1,112 @@
+import time
+import csv
+import serial
+from drivers.caen.NDT1470 import NDT1470
+from drivers.tektronix.TDS2024B import TDS2024B
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from tqdm import tqdm
+
+import pandas as pd
+
+SERIAL_PORT = 'COM5'  # Change as needed
+
+
+voltages = []
+currents = []
+time_stamps = []
+v_p2ps_ch1 = []
+v_p2ps_ch2 = []
+v_rmss_ch1 = []
+v_rmss_ch2 = []
+
+
+hv = NDT1470(SERIAL_PORT)
+scope = TDS2024B("USB0::0x0699::0x036A::C100158::INSTR")
+print(scope.query_idn())
+
+fig = plt.figure()
+gs = fig.add_gridspec(2, 2)
+ax = [
+ fig.add_subplot(gs[:, 0]),
+ fig.add_subplot(gs[0, 1]),
+ fig.add_subplot(gs[1, 1])
+]
+
+# fig, ax = plt.subplots( nrows=1, ncols=2 )  # create figure & 1 axis
+
+
+def update_plot():
+    voltage = hv.queryVMON(3)
+    # print(voltage)
+    current = hv.queryIMON(3)
+    # print(current)
+    v_p2p_ch1 = scope.query_Vp2p(1)
+    v_rms_ch1 = scope.query_Vrms(1)
+    v_p2p_ch2 = scope.query_Vp2p(2)
+    v_rms_ch2 = scope.query_Vrms(2)
+    
+    voltages.append(voltage)
+    currents.append(current)
+    v_p2ps_ch1.append(v_p2p_ch1)
+    v_rmss_ch1.append(v_rms_ch1)
+    v_p2ps_ch2.append(v_p2p_ch2)
+    v_rmss_ch2.append(v_rms_ch2)
+    time_stamps.append(time.time())
+    
+    ax[0].clear()
+    ax[0].scatter(currents, voltages, label='V(I)', s=3)
+    
+    ax[0].set_xlabel(r'Current ($\mu A$)')
+    ax[0].set_ylabel(r'Voltage ($V$)')
+    ax[0].legend()
+
+    ax[1].clear()
+    ax[1].scatter(voltages, v_p2ps_ch1, label=r'$V_{p2p}(V)$ - CH1', color = "blue", s=3)
+    ax[1].scatter(voltages, v_p2ps_ch2, label=r'$V_{p2p}(V)$ - CH2', color = 'green', s=3)
+    
+    ax[1].set_xlabel(r'Voltage ($V$)')
+    ax[1].set_ylabel(r'Noise p-p ($V$)')
+    ax[1].legend()
+    
+    ax[2].clear()
+    ax[2].scatter(voltages, v_rmss_ch1, label=r'$V_{rms}(V)$ - CH1', color = 'lightblue', s=3)
+    ax[2].scatter(voltages, v_rmss_ch2, label=r'$V_{rms}(V)$ - CH2', color = 'lightgreen', s=3)
+    
+    ax[2].set_xlabel(r'Voltage ($V$)')
+    ax[2].set_ylabel(r'Noise RMS ($V$)')
+    ax[2].legend()
+
+    plt.draw()
+    plt.pause(0.01)
+
+
+start_voltage = 700
+end_voltage = 2285
+delta_voltage = 5
+
+hv.setVSET(3, start_voltage)
+hv.setCHOn(3)
+time.sleep(8)
+
+# Example usage: Vary voltage from 1V to 5V
+# plt.show()
+for v in tqdm(range(start_voltage, end_voltage, delta_voltage)):
+    hv.setVSET(3, v)
+    time.sleep(2)
+    update_plot()
+
+hv.setCHOff(3)
+
+
+df = pd.DataFrame(
+    {'time': time_stamps,
+     'pmt_high_voltage': voltages, 
+     'pmt_current': currents,
+     'v_p2p_ch1': v_p2ps_ch1,
+     'v_rms_ch1': v_rmss_ch1,
+     'v_p2p_ch2': v_p2ps_ch2,
+     'v_rms_ch2': v_rmss_ch2})
+
+df.to_csv(f'analysis/data/dark_pmt/I_V_noise_{start_voltage}_{end_voltage}V.csv', index=False)
+plt.show()
---
+++ drivers/tektronix/TDS2024B.py
@@ -0,0 +1,98 @@
+import pyvisa 
+import time
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import struct
+
+class TDS2024B:
+    def __init__(self, device_id, debug=False, visa_backend=r"C:/Windows/System32/visa64.dll"):
+        self.device_id = device_id
+        self.rm = pyvisa.ResourceManager(visa_backend)
+        self.handle = self.rm.open_resource(device_id)
+        
+        self.debug = debug
+
+        if self.debug:
+            pyvisa.log_to_screen()
+    
+    def query(self, command):
+        return self.handle.query(command)
+    
+    def write(self, command):
+        self.handle.write(command)
+    
+    def read(self):
+        return self.handle.read()
+    
+    def query_idn(self):
+        return self.query("*IDN?")
+    
+    def select_measurement_channel(self, channel):
+        if channel not in [1, 2, 3, 4]:
+            print("Error: Invalid channel number")
+            return None
+        command = f"MEASUrement:IMMed:SOUrce CH1"
+        self.write(command)
+
+    def select_data_channel(self, channel):
+        if channel not in [1, 2, 3, 4]:
+            print("Error: Invalid channel number")
+            return None
+        command = f"DATA:SOURCE CH{channel}"
+        self.write(command)
+    
+    def query_Vp2p(self, channel):
+        self.select_measurement_channel(channel)
+        command1 = "MEASUrement:IMMed:TYPe PK2PK"
+        self.write(command1)
+        command2 = "MEASUrement:IMMed:VALue?"
+        res = self.query(command2)
+        return float(res)
+    
+    def query_Vrms(self, channel):
+        self.select_measurement_channel(channel)
+        command1 = "MEASUrement:IMMed:TYPe RMS"
+        self.write(command1)
+        command2 = "MEASUrement:IMMed:VALue?"
+        res = self.query(command2)
+        return float(res)
+    
+    def query_Waveform(self, channel):
+        # Set data encoding to ASCII
+        self.select_data_channel(channel)
+
+        self.write("DATA:ENC RIB")
+        self.write("DATA:WIDTH 1")
+        self.write("DATA:START 1")
+
+        x_increment = float(self.query("WFMPRE:XINCR?"))  
+        x_origin = float(self.query("WFMPRE:XZERO?"))  
+        y_multiplier = float(self.query("WFMPRE:YMULT?"))  
+        y_offset = float(self.query("WFMPRE:YOFF?"))  
+        y_zero = float(self.query("WFMPRE:YZERO?"))  
+
+        self.write("CURVE?")
+        raw_data = self.handle.read_raw()  
+
+        header_length = int(raw_data[1])  
+        waveform_data = raw_data[2 + header_length:-1]  
+        data_points = np.array(struct.unpack(f"{len(waveform_data)}B", waveform_data))
+
+        voltages = (data_points - y_offset) * y_multiplier + y_zero
+        time_values = np.arange(0, len(voltages)) * x_increment + x_origin
+
+        df = pd.DataFrame({"t": time_values, "V": voltages})
+        return df
+
+
+if __name__ == '__main__':
+    # Open serial connection
+    tds = TDS2024B("USB0::0x0699::0x036A::C100158::INSTR", debug=False)
+    print(tds.query_idn())
+    print(tds.query_Vp2p(2))
+    print(tds.query_Vrms(2))
+    df = tds.query_Waveform(2)
+    plt.plot(df["t"], df["V"])
+    plt.show()
+
